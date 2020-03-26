6e2a1fdd29ba40b016d6767926c953ace9b5b1f5
Chinmay Naphade <chinmay.naphade@gmail.com>
2020-03-26 13:00:07 -0400

    Setting up React App

---
+++ orders-app-client/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Order</h1>
+        <p>Futuralis Orders App</p>
+      </div>
+    </div>
+  );
+}
